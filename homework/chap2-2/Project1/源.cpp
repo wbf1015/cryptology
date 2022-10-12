9ad784c3f01db1b924ee0e3fde9e21382148d9d4
Bofan_Wei <2011395@mail.nankai.edu.cn>
2022-10-13 01:13:50 +0800

    2022-10-13

---
+++ homework/chap2-2/Project1/源.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include<map>
+using namespace std;
+int store[49] = { 1,1,1,0,0,0,0,0,1,0,1,1,1,1,1,1,0,0,0,1,1,1,0,1,1,1,1,1,1,0,0,1,1,0,0,0,1,1,1,1,0,0,0,0,1,0,0,1,1 };
+map <int, int>ans;
+int main() {
+    for (int i = 0; i <= 48; i++) {
+        int same = 0;
+        int diff = 0;
+        for (int j = 0; j <= 48; j++) {
+            if (store[j] == store[(j + i) % 49]) { same++; }
+            else { diff++; }
+        }
+        int res = same - diff;
+        cout << "位移为" << i << "时" << "异构自相关函数的分母为" << res << endl;
+        if (ans.find(res) == ans.end()) {
+            ans.insert(pair<int, int>(res, 1));
+        }
+        else {
+            ans[res] = ans[res] + 1;
+        }
+    }
+    for (auto it : ans) {
+        cout << it.first << " " << it.second << endl;
+    }
+}
