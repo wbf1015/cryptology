af5b25e84183d229f88acd8894a7b14660c5709c
Bofan_Wei <2011395@mail.nankai.edu.cn>
2022-11-11 21:21:00 +0800

    2022-11-11

--- 实验1/mylab1/源.cpp
+++ 实验1/mylab1/源.cpp
@@ -1,8 +1,12 @@
 #include<iostream>
-#include<string>
+#include<cstring>
 #include<map>
 #include<vector>
+#include<fstream>
 using namespace std;
+float *ftable=new float [26];
+char *frank=new char[26];
+
 
 //获取长度
 int get_length(char* text) {
@@ -11,6 +15,16 @@ int get_length(char* text) {
         i++;
     }
     return i;
+}
+
+int get_length_without_blank(char* text) {
+    int i = 0;
+    int count = 0;
+    while (text[i] != '\0') {
+        i++;
+        count++;
+    }
+    return count;
 }
 
 //移位密码
@@ -134,8 +148,138 @@ int singleTableChange(){
     return 0;
 }
 
+void initialftable(){
+    ftable['e' - 97] = 11.67; frank[0] = 'e';
+    ftable['t' - 97] = 9.53;  frank[1] = 't';
+    ftable['o'-97]=8.22;  frank[2]='o';
+    ftable['i'-97]=7.81;  frank[3]='i';
+    ftable['a'-97]=7.73;  frank[4]='a';
+    ftable['n'-97]=6.71;  frank[5]='n';
+    ftable['s'-97]=6.55;  frank[6]='s';
+    ftable['r'-97]=5.97;  frank[7]='r';
+    ftable['h'-97]=4.52;  frank[8]='h';
+    ftable['l'-97]=4.3;   frank[9]='l';
+    ftable['d'-97]=3.24;  frank[10]='d';
+    ftable['u'-97]=3.21;  frank[11]='u';
+    ftable['c'-97]=3.06;  frank[12]='c';
+    ftable['m'-97]=2.8;   frank[13]='m';
+    ftable['p'-97]=2.34;  frank[14]='p';
+    ftable['y'-97]=2.22;  frank[15]='y';
+    ftable['f'-97]=2.14;  frank[16]='f';
+    ftable['g'-97]=2.00;  frank[17]='g';
+    ftable['w'-97]=1.69;  frank[18]='w';
+    ftable['b'-97]=1.58;  frank[19]='b';
+    ftable['v'-97]=1.03;  frank[20]='v';
+    ftable['k'-97]=0.79;  frank[21]='k';
+    ftable['x'-97]=0.3;   frank[22]='x';
+    ftable['j'-97]=0.23;  frank[23]='j';
+    ftable['q'-97]=0.12;  frank[24]='q';
+    ftable['z'-97]=0.09;  frank[25]='z';
+}
+char findmax(map<char,double>&map) {
+    char max_char;
+    double max=0;
+    for (auto it : map) {
+        if (it.second >= max) {
+            max_char = it.first;
+            max = it.second;
+        }
+    }
+    map.erase(max_char);
+    return max_char;
+}
+
+
+int attackSingleTableChange(){
+    initialftable();
+    char *message=new char[1000];
+    ifstream infile;
+    infile.open("message1.txt",ios::in);
+    if(!infile.is_open()){
+        cout<<"读取文件失败"<<endl;
+        return -1;
+    }
+    char buff[1000];
+    while(infile.getline(buff,1000)){
+        for (int i = 0; i < 1000; i++) {
+            if (buff[i] != '\0') {
+                message[i] = buff[i];
+                continue;
+            }
+            if (buff[i] == '\0') {
+                message[i] = '\0';
+                break;
+            }
+        }
+    }
+    int n = get_length(message);
+    int cn = get_length_without_blank(message);
+    cout<<"您要攻击的密文长度为：" << get_length(message) << endl;
+    cout << "您要攻击的密文信息为：" << message << endl;
+    map<char, double>temprecord;
+    map<char, double>record;//记录中每个字符串出现频率
+    //记录每个字符出现的次数并转换为小写存储便于识别
+    for (int i = 0; i < n; i++) {
+        char temp;
+        if (message[i] >= 65 && message[i] <= 90) {
+            temp = message[i] + 32;
+            message[i] = temp;
+        }else{
+            if (message[i] >= 97 && message[i] <= 122) {
+                temp = message[i];
+            }
+            else {
+                continue;
+            }
+        }
+        if (temprecord.find(temp) != temprecord.end()) {
+            temprecord[temp] += 1;
+            record[temp] += 1;
+            continue;
+        }
+        else {
+            temprecord.insert(pair<char, double>(temp, 1.0));
+            record.insert(pair<char, double>(temp, 1.0));
+        }
+    }
+    cout << message << endl;
+    //找那些没出现的
+    for (int i = 97; i <= 122; i++) {
+        if (record.find(char(i)) == record.end()) {
+            record.insert(pair<char, char>(char(i), 0.0));
+            temprecord.insert(pair<char, char>(char(i), 0.0));
+        }
+    }
+    char myrank[26];//密文中字符串排名
+    for (int i = 0; i < 26; i++) {
+        myrank[i] = findmax(temprecord);
+    }
+    //展示单表代换对比结果
+    cout << "下面展示初步单表代换结果" << endl;
+    cout << "统计字符" << "    " << "统计字符频率" << "    " << "密文字符" << "    " << "密文字符频率" << endl;
+    for (int i = 0; i < 26; i++) {
+        cout <<"  " << frank[i] << "           " << ftable[frank[i] - 97] << "           " << myrank[i] << "           " << (record.find(myrank[i])->second / cn) * 100 << endl;;
+    }
+    //构造代换表
+    map<char, char>stctable;
+    for (int i = 0; i < 26; i++) {
+        stctable.insert(pair<char, char>(frank[i], myrank[i]));
+    }
+    cout << endl;
+    for (auto it : stctable) {
+        cout << it.first << " " << it.second << endl;
+    }
+    cout << endl;
+    cout << "根据频率的单表代换结果解密如下：" << endl;
+    for (int i = 0; i < n; i++) {
+        if (message[i] >= 97 && message[i] <= 122) { cout << stctable[message[i]]; }
+        else { cout << message[i]; }
+    }
+}
+
 int main(){
     //shiftword();
     //attackshift();
-    singleTableChange();
+    //singleTableChange();
+    attackSingleTableChange();
 }
