f066785011d4643ee415f2d5bd228554130f4156
Bofan_Wei <2011395@mail.nankai.edu.cn>
2022-12-27 16:20:05 +0800

    finish MD5

--- 实验五/MD5/MD5.h
+++ 实验五/MD5/MD5.h
@@ -90,6 +90,66 @@ string Hex2Bin(string s) {
     return ret;
 }
 
+string Dec2Hex(int n) {
+    string s;
+    string t = "0123456789ABCDEF";
+    int x;
+    while (n != 0) {
+        x = n % 16;
+        // 将 n % 16 转换为字符逆序存入 s 
+        s = t[x] + s;
+        n = n / 16;
+    }
+
+    if (s == "") {
+        cout << "错误的十进制转16进制输入" << endl;
+        return s;
+    }
+    else {
+        return s;
+    }
+}
+
+string Text2Hex(string s) {
+    string ret;
+    for (int i = 0; i < s.size(); i++) {
+        int t = s[i];
+        string ss = Dec2Hex(t);
+        ret.append(ss);
+    }
+    return ret;
+}
+
+string Bin2Hex(string s) {
+    if (s.size() % 4 != 0) {
+        reverse(s.begin(), s.end());
+        for (int i = 1; i <= 4-(s.size() % 4); i++) {
+            s.append(string(1, '0'));
+        }
+        reverse(s.begin(), s.end());
+    }
+    string ret;
+    for (int i = 0; i < s.size(); i += 4) {
+        if (s[i] == '0' && s[i + 1] == '0' && s[i + 2] == '0' && s[i + 3] == '0') { ret.append(string(1, '0')); continue; }
+        if (s[i] == '0' && s[i + 1] == '0' && s[i + 2] == '0' && s[i + 3] == '1') { ret.append(string(1, '1')); continue; }
+        if (s[i] == '0' && s[i + 1] == '0' && s[i + 2] == '1' && s[i + 3] == '0') { ret.append(string(1, '2')); continue; }
+        if (s[i] == '0' && s[i + 1] == '0' && s[i + 2] == '1' && s[i + 3] == '1') { ret.append(string(1, '3')); continue; }
+        if (s[i] == '0' && s[i + 1] == '1' && s[i + 2] == '0' && s[i + 3] == '0') { ret.append(string(1, '4')); continue; }
+        if (s[i] == '0' && s[i + 1] == '1' && s[i + 2] == '0' && s[i + 3] == '1') { ret.append(string(1, '5')); continue; }
+        if (s[i] == '0' && s[i + 1] == '1' && s[i + 2] == '1' && s[i + 3] == '0') { ret.append(string(1, '6')); continue; }
+        if (s[i] == '0' && s[i + 1] == '1' && s[i + 2] == '1' && s[i + 3] == '1') { ret.append(string(1, '7')); continue; }
+        if (s[i] == '1' && s[i + 1] == '0' && s[i + 2] == '0' && s[i + 3] == '0') { ret.append(string(1, '8')); continue; }
+        if (s[i] == '1' && s[i + 1] == '0' && s[i + 2] == '0' && s[i + 3] == '1') { ret.append(string(1, '9')); continue; }
+        if (s[i] == '1' && s[i + 1] == '0' && s[i + 2] == '1' && s[i + 3] == '0') { ret.append(string(1, 'A')); continue; }
+        if (s[i] == '1' && s[i + 1] == '0' && s[i + 2] == '1' && s[i + 3] == '1') { ret.append(string(1, 'B')); continue; }
+        if (s[i] == '1' && s[i + 1] == '1' && s[i + 2] == '0' && s[i + 3] == '0') { ret.append(string(1, 'C')); continue; }
+        if (s[i] == '1' && s[i + 1] == '1' && s[i + 2] == '0' && s[i + 3] == '1') { ret.append(string(1, 'D')); continue; }
+        if (s[i] == '1' && s[i + 1] == '1' && s[i + 2] == '1' && s[i + 3] == '0') { ret.append(string(1, 'E')); continue; }
+        if (s[i] == '1' && s[i + 1] == '1' && s[i + 2] == '1' && s[i + 3] == '1') { ret.append(string(1, 'F')); continue; }
+    }
+    return ret;
+}
+
 void string2vectorChar(string s, vector<char>&c) {
     c.clear();
     for (int i = 0; i < s.size(); i++) {
@@ -99,50 +159,13 @@ void string2vectorChar(string s, vector<char>&c) {
 }
 
 void readcmd(vector<char>& v) {
-    int choose;
-    cout << "选择不同的输入方式: 1代表输入二进制，2代表输入十进制，3代表输入16进制" << endl;
-    cout << "温馨提示：输入的二进制和16进制会作为文本字符串进行计算，输入的十进制数字将根据数值转换为二进制文本字符串计算" << endl;
-    cin >> choose;
-    if (choose == 1) {
-        string s;
-        cin >> s;
-        if (checkBin(s)) {
-            cout << "成功输入二进制文本" << endl;
-            string2vectorChar(s, v);
-            return;
-        }
-        else {
-            cout << "输入错误" << endl;
-            return;
-        }
-    }
-    else {
-        if (choose == 2) {
-            int i;
-            cin >> i;
-            string s = Dec2Bin(i);
-            cout << "成功读取10进制数值并转换" << endl;
-            string2vectorChar(s, v);
-            return;
-        }
-        else {
-            if (choose == 3) {
-                string s;
-                cin >> s;
-                if (!checkHex(s)) { cout << "错误的输入" << endl; return; }
-                s = Hex2Bin(s);
-                cout << "成功去读16进制文本并转换为2进制" << endl;
-                string2vectorChar(s, v);
-                return;
-            }
-            else {
-                cout << "错误的输入" << endl;
-                return;
-            }
-        }
-    }
-    
-
+    cout << "请输入文本字符串" << endl;
+    char c[10000];
+    cin.get(c, 10000);
+    string s(c);
+    s = Text2Hex(s);
+    s = Hex2Bin(s);
+    string2vectorChar(s, v);
 }
 
 //完成第一步填充
@@ -180,6 +203,23 @@ string smallEnd(string s) {
     for (int i = 7; i >= 0; i--) {
         ret.append(vs[i]);
     }
+    return ret;
+}
+
+string smallEndRegister(string s) {
+    string ret;
+    if (s.size() != 32) {
+        cout << "寄存器小端转换字符错误" << endl;
+        return ret;
+    }
+    string s1 = s.substr(0, 8);
+    string s2 = s.substr(8, 8);
+    string s3 = s.substr(16, 8);
+    string s4 = s.substr(24, 8);
+    ret.append(s4);
+    ret.append(s3);
+    ret.append(s2);
+    ret.append(s1);
     return ret;
 }
 
@@ -297,7 +337,7 @@ string F(string b, string c, string d) {
 string G(string b, string c, string d) {
     string ret;
     if (b.size() != 32 || c.size() != 32 || d.size() != 32) {
-        cout << "F函数参数错误" << endl;
+        cout << "G函数参数错误" << endl;
         return ret;
     }
     for (int i = 0; i < 32; i++) {
@@ -316,7 +356,7 @@ string G(string b, string c, string d) {
 string H(string b, string c, string d) {
     string ret;
     if (b.size() != 32 || c.size() != 32 || d.size() != 32) {
-        cout << "F函数参数错误" << endl;
+        cout << "H函数参数错误" << endl;
         return ret;
     }
     for (int i = 0; i < 32; i++) {
@@ -335,7 +375,7 @@ string H(string b, string c, string d) {
 string I(string b, string c, string d) {
     string ret;
     if (b.size() != 32 || c.size() != 32 || d.size() != 32) {
-        cout << "F函数参数错误" << endl;
+        cout << "I函数参数错误" << endl;
         return ret;
     }
     for (int i = 0; i < 32; i++) {
@@ -359,7 +399,7 @@ string CLS(string s,int i) {
     }
     for (int j = 0; j < 32; j++) {
         char c;
-        if (j + i > 32) {
+        if (j + i >= 32) {
             c = s[j + i - 32];
         }
         else {
@@ -373,6 +413,7 @@ string CLS(string s,int i) {
 class myMD5 {
 public:
     vector<string>text;
+    vector<string>smallText;
     myMD5(vector<char>&vc);
     myMD5(const myMD5& m);
     int getGroupSize()const { return text.size() / 16; }//总共有几组，但是组的下标是从0开始的
@@ -380,6 +421,8 @@ public:
     string getGroup(int i);
     string getWord(int i);
     friend ostream& operator<<(ostream& out, const myMD5& m);
+    void printSmallText();
+    void getSmallText();
 };
 myMD5::myMD5(vector<char>&vc) {
     for (int i = 0; i < vc.size(); i += 32) {
@@ -404,20 +447,39 @@ string myMD5::getGroup(int i) {
     else {
         string s;
         for (int j = 0; j < 16; j++) {
-            s.append(this->text[i * 16 + j]);
+            s.append(this->smallText[i * 16 + j]);
         }
         return s;
     }
 }
 string myMD5::getWord(int i) {
-    if (i >= this->getGroupSize()) {
+    if (i >= this->getWordSize()) {
         string s;
         cout << "错误的字号" << endl;
         return s;
     }
     else {
         string s;
-        s = this->text[i];
+        s = this->smallText[i];
+        return s;
+    }
+}
+void myMD5::getSmallText() {
+    for (int i = 0; i < text.size(); i++) {
+        string s = text[i];
+        string s1 = s.substr(0, 8);
+        string s2 = s.substr(8, 8);
+        string s3 = s.substr(16, 8);
+        string s4 = s.substr(24, 8);
+        s.clear();
+        s.append(s4); s.append(s3); s.append(s2); s.append(s1);
+        smallText.push_back(s);
+    }
+}
+void myMD5::printSmallText() {
+    cout << "MD5的小端序表示为" << endl;
+    for (int i = 0; i < smallText.size(); i++) {
+        cout << smallText[i] << endl;
     }
 }
 ostream& operator<<(ostream& out, const myMD5& m) {
@@ -426,10 +488,108 @@ ostream& operator<<(ostream& out, const myMD5& m) {
     }
     return out;
 }
+void Myround(int q, int round, myMD5& m);
+void step(int q, int round, int i, myMD5& m);
 
 void InitialRegister() {
     string sa = Hex2Bin("67452301");
     string sb = Hex2Bin("EFCDAB89");
     string sc = Hex2Bin("98BADCFE");
     string sd = Hex2Bin("10325476");
+    registerA = sa;
+    registerB = sb;
+    registerC = sc;
+    registerD = sd;
+}
+
+string HashMD5(myMD5& m) {
+    InitialRegister();
+    m.getSmallText();
+    m.printSmallText();
+    for (int k = 0; k < m.getGroupSize(); k++) {
+        string ra = registerA;
+        string rb = registerB;
+        string rc = registerC;
+        string rd = registerD;
+        for (int j = 1; j <= 4; j++) {
+            Myround(k, j, m);
+        }
+        registerA = stringADD(registerA, ra);
+        registerB = stringADD(registerB, rb);
+        registerC = stringADD(registerC, rc);
+        registerD = stringADD(registerD, rd);
+    }
+    string ret;
+    ret.append(smallEndRegister(registerA));
+    ret.append(smallEndRegister(registerB));
+    ret.append(smallEndRegister(registerC));
+    ret.append(smallEndRegister(registerD));
+    return ret;
+}
+
+
+//这个就是每一轮
+void Myround(int q, int round, myMD5& m) {
+    for (int i = 1; i <= 16; i++) {
+        step(q, round, i, m);
+    }
+}
+
+//q代表着是第几个512比特的分组，round代表着是第几轮，i代表着是这一轮的第几步
+//round的取值范围是1-4 step是1-16
+//q应该是从0开始的
+void step(int q, int round, int i,myMD5 &m) {
+    //把bcd寄存器和逻辑函数的取值计算出来
+    string ADDbcd;
+    if (round == 1) {
+        ADDbcd = F(registerB, registerC, registerD);
+    }
+    if (round == 2) {
+        ADDbcd = G(registerB, registerC, registerD);
+    }
+    if (round == 3) {
+        ADDbcd = H(registerB, registerC, registerD);
+    }
+    if (round == 4) {
+        ADDbcd = I(registerB, registerC, registerD);
+    }
+    //A和逻辑函数的取值
+    string Add1;
+    Add1 = stringADD(registerA, ADDbcd);
+    //然后把X[k]取出来
+    int k;
+    if (round == 1) { k = i-1; }
+    if (round == 2) { k = round2(i-1); }
+    if (round == 3) { k = round3(i-1); }
+    if (round == 4) { k = round4(i-1); }
+    //完成对于Xk的加法
+    string Xk = m.getWord(q*16+k);
+    string Add2 = stringADD(Xk, Add1);
+    //完成对于Ti的加法
+    string Ti = T[16 * (round - 1)+i];
+    Ti = Hex2Bin(Ti);
+    string Add3 = stringADD(Ti, Add2);
+    //完成对于ADD3的移位
+    string moveS = CLS(Add3, clss[round][i]);
+    string newA = stringADD(moveS, registerB);
+    //完成寄存器的交换
+    string ra = registerA;
+    string rb = registerB;
+    string rc = registerC;
+    string rd = registerD;
+    registerA = rd;
+    registerB = newA;
+    registerC = rb;
+    registerD = rc;
+
+    cout << "第" << round << "轮,第" << i << "步后经过寄存器交换后的值为" << endl;
+    cout << "k=" << k << endl;
+    cout << Bin2Hex(registerA) << endl;
+    //cout << registerA << endl;
+    cout << Bin2Hex(registerB) << endl;
+    //cout << registerB << endl;
+    cout << Bin2Hex(registerC) << endl;
+    //cout << registerC << endl;
+    cout << Bin2Hex(registerD) << endl;
+    cout << "------------------------------------------------------------------------" << endl;
 }
--- 实验五/MD5/源.cpp
+++ 实验五/MD5/源.cpp
@@ -55,6 +55,37 @@ void testAddString() {
 	cout << b << endl;
 	cout << c << endl;
 }
+void testCLS() {
+	string s = "10010001001110110110010100110110";
+	cout << CLS(s, 31);
+}
+void testHash() {
+	vector<char>vc;
+	readcmd(vc);
+	cout << "读入的文件为：" << endl;
+	printVectorChar(vc);
+	vector<char>vc2(vc);
+
+	fill(vc);
+	cout << "填充后的大小为:";
+	cout << vc.size() << endl;
+	cout << "填充后的内容为：" << endl;
+	printVectorChar(vc);
+
+	fillLength(vc, vc2);
+	cout << "最终填充后的大小为：" << endl;
+	cout << vc.size() << endl;
+	cout << "最终填充后的内容为：" << endl;
+	printVectorChar(vc);
+
+	myMD5 m(vc);
+	cout << "进入myMD5后的结果为：" << endl;
+	cout << m;
+
+	string ret=HashMD5(m);
+	cout << "最后的结果为：" << ret << endl;
+	cout << "16进制表示为" << Bin2Hex(ret) << endl;
+}
 int main() {
-	
+	testHash();
 }
